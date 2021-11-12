7db0ed1050da13f3c834ff38e9df8199aa5d72f1
sperkins-ag <76228662+sperkins-ag@users.noreply.github.com>
2021-11-12 10:47:32 -0700

    First Commit

--- target/2.2.0/photon/obj/src/user_module.o.d
+++
@@ -1,4 +0,0 @@
-/Users/scottperkins/Documents/WeatherStation/blink/target/2.2.0/photon//obj/src/user_module.o: \
- src/user_module.c ../../../dynalib/inc/dynalib.h
-
-../../../dynalib/inc/dynalib.h:
